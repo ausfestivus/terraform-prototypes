545c3fd433c57800f06305977896275a256c7965
Andrew Best <40222285+ausfestivus@users.noreply.github.com>
2024-04-14 13:52:33 +1000

    WIP - use seal-status as path for noop resource

--- terraform-vault-dynamic-credentials/noop.tf
+++ terraform-vault-dynamic-credentials/noop.tf
@@ -1,11 +1,10 @@
 # We use a Vault Provider data resource as a no-op test of the Vault provider config.
 
-data "vault_generic_secret" "health" {
-  path = "/sys/health"
+data "vault_generic_secret" "seal_status" {
+  path = "/sys/seal-status"
 }
 
-# Now display the config
+# Now display the seal status output
 output "sys_health" {
-  value     = data.vault_generic_secret.health
-  sensitive = true
+  value = data.vault_generic_secret.seal_status
 }
